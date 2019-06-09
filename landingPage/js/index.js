ca0194a6895af14153a54cb8d4baf3ce7a3b160f
helios1101 <harshit.singh1101@gmail.com>
2019-06-09 05:47:52 +0530

    uniformity resolved

--- landingPage/js/index.js
+++ landingPage/js/index.js
@@ -1,19 +1,29 @@
 (function() {
-  
-  var slideContainer = $('.slide-container');
-  
+  var slideContainer = $(".slide-container");
+
   slideContainer.slick();
-  
-  $('.clash-card__image img').hide();
-  $('.slick-active').find('.clash-card img').fadeIn(200);
-  
+
+  $(".clash-card__image img").hide();
+  $(".slick-active")
+    .find(".clash-card img")
+    .fadeIn(200);
+
   // On before slide change
-  slideContainer.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-    $('.slick-active').find('.clash-card img').fadeOut(1000);
+  slideContainer.on("beforeChange", function(
+    event,
+    slick,
+    currentSlide,
+    nextSlide
+  ) {
+    $(".slick-active")
+      .find(".clash-card img")
+      .fadeOut(1000);
   });
-  
+
   // On after slide change
-  slideContainer.on('afterChange', function(event, slick, currentSlide) {
-    $('.slick-active').find('.clash-card img').fadeIn(200);
+  slideContainer.on("afterChange", function(event, slick, currentSlide) {
+    $(".slick-active")
+      .find(".clash-card img")
+      .fadeIn(200);
   });
 })();
